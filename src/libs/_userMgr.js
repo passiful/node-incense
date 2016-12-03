3bf031f8368d9daf5d410b5508f83b3cf11ca6cf
Tomoya Koyanagi <tomk79@gmail.com>
2016-12-03 17:42:46 +0900

    logoutのエラー対策

--- src/libs/_userMgr.js
+++ src/libs/_userMgr.js
@@ -22,15 +22,22 @@ module.exports = function( app, $timelineList, $field, $fieldInner ){
 	 * ユーザー情報を削除する
 	 */
 	this.logout = function(connectionId, callback){
-		callback = callback || function(err, userInfo){};
-		var rtn = userConnectionList[connectionId];
+		callback = callback || function(err, {}){};
+		try {
+			var rtn = userConnectionList[connectionId];
 
-		userList[rtn.id] = undefined;
-		delete( userList[rtn.id] );
-		userConnectionList[connectionId] = undefined;
-		delete( userConnectionList[connectionId] );
+			userList[rtn.id] = undefined;
+			delete( userList[rtn.id] );
+			userConnectionList[connectionId] = undefined;
+			delete( userConnectionList[connectionId] );
 
-		callback( null, rtn );
+			callback( null, rtn );
+			return;
+		} catch (e) {
+			console.error('[ERROR] failed to logout: '+connectionId, rtn);
+			callback('[ERROR] failed to logout: '+connectionId, rtn);
+			return;
+		}
 		return;
 	}
 
