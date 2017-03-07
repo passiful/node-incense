84bfc73b1f3e9216d384b6b5d1e2af7e45cf54ab
Tomoya Koyanagi <tomk79@gmail.com>
2017-03-07 20:33:43 +0900

    (update test)
    コンストラクタの仕様変更に伴うテストコード変更

--- tests/mainTest.js
+++ tests/mainTest.js
@@ -3,13 +3,25 @@ var path = require('path');
 var fs = require('fs');
 var utils79 = require('utils79');
 var Promise = require("es6-promise").Promise;
-var Inscnse = require('../libs/main.js');
-var incense = Inscnse.getBifloraMain({
-	'dataDir': __dirname + '/data/',
-	'db': {
+var Incense = require('../libs/main.js');
+console.log(Incense);
+var incense = new Incense({
+	'dataDir': __dirname + '/data/', // <- data directory (Read/Write permission required)
+	'db': { // <- database setting
 		"dbms": "sqlite",
 		"storage": __dirname + "/data/__database.sqlite",
 		"tablePrefix": "incense"
+	} ,
+	'getUserInfo': function( socket, clientDefaultUserInfo, callback ){
+		// provide user info.
+		// e.g. {
+		//     'id': 'user_id',
+		//     'name': 'User Name',
+		//     'url': 'http://example.com/',
+		//     'icon': 'http://example.com/sample.png'
+		// }
+		callback(clientDefaultUserInfo);
+		return;
 	}
 });
 
@@ -17,9 +29,9 @@ describe('Basic', function() {
 
 	it("define", function(done) {
 		this.timeout(60*1000);
-		assert.strictEqual(typeof(Inscnse), typeof(function(){}));
-		assert.strictEqual(typeof(Inscnse.getBifloraApi), typeof(function(){}));
-		assert.strictEqual(typeof(Inscnse.getBifloraMain), typeof(function(){}));
+		assert.strictEqual(typeof(Incense), typeof(function(){}));
+		assert.strictEqual(typeof(incense.getBifloraApi), typeof(function(){}));
+		assert.strictEqual(typeof(incense.getBifloraMain), typeof(function(){}));
 		done();
 	});
 
@@ -36,17 +48,18 @@ describe('Creating new board', function() {
 	it("divide board ID", function(done) {
 		this.timeout(60*1000);
 		// console.log(incense);
-		assert.strictEqual(incense.dbh.divideBoardId('abcdefg'), 'ab/cd/ef/g');
-		assert.strictEqual(incense.dbh.divideBoardId('a'), 'a');
-		assert.strictEqual(incense.dbh.divideBoardId('ab'), 'ab');
-		assert.strictEqual(incense.dbh.divideBoardId('abcd'), 'ab/cd');
+		var bifloraMain = incense.getBifloraMain();
+		assert.strictEqual(bifloraMain.dbh.divideBoardId('abcdefg'), 'ab/cd/ef/g');
+		assert.strictEqual(bifloraMain.dbh.divideBoardId('a'), 'a');
+		assert.strictEqual(bifloraMain.dbh.divideBoardId('ab'), 'ab');
+		assert.strictEqual(bifloraMain.dbh.divideBoardId('abcd'), 'ab/cd');
 		done();
 	});
 
 	it("create", function(done) {
 		this.timeout(60*1000);
 		// console.log(incense);
-		incense.board.createNewBoard({"title": "TEST BOARD"}, function(newBoardId){
+		incense.getBifloraMain().board.createNewBoard({"title": "TEST BOARD"}, function(newBoardId){
 			console.log(newBoardId);
 
 			assert.strictEqual(typeof(incense), typeof({}));
